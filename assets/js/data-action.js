8d8b66686872206fa3d6a7b20e0f167ac3d5f1ab
HyoungJaeJeong <fss1999@kyobodts.co.kr>
2024-11-26 23:36:28 +0900

    인구현황 적용

--- assets/js/data-action.js
+++ assets/js/data-action.js
@@ -21,5 +21,36 @@ $(document).ready(function () {
     $('.btn01_ss input[type="submit"]').click(function (event) {
         event.preventDefault();
         $(".guide-wrap, .data-wrap").addClass("on");
+
+        // 선택된 부모 지역 (시/도)
+        const selectedParent = $('#region-parent').val();
+        // 선택된 자식 지역 (군/구)
+        const selectedRegion = $('#region-name').val();
+
+        // 선택된 지역에 해당하는 데이터를 필터링
+        const regionData = regions.find(region =>
+            region.parent === selectedParent && region.name === selectedRegion
+        );
+
+        if (!regionData) {
+            alert('해당 지역의 데이터를 찾을 수 없습니다.');
+            return false;
+        }
+
+        // 데이터가 존재하는 경우 원하는 동작 수행
+        console.log(regionData);
+
+        // 인구 현황
+        $('h4.content_title').html(`${selectedParent} ${selectedRegion}의 주요 특징`);
+        $('#populationStatus-population').html(`${regionData.populationStatus.population['2023'].toLocaleString()}`);
+        $('#populationStatus-forestryPopulation').html(`${regionData.populationStatus.forestryPopulation['2020'].toLocaleString()}`);
+        $('#populationStatus-households').html(`${regionData.populationStatus.households['2023'].toLocaleString()}`);
+        $('#populationStatus-forestryHouseholds').html(`${regionData.populationStatus.forestryHouseholds['2020'].toLocaleString()}`);
+        $('#populationStatus-averageAge').html(`${regionData.populationStatus.averageAge['2023']}`);
+        displayPeopleChart(regionData.regionCharacteristics);
+
+        // 기타 추가 데이터 출력
+        //$('#productsProduction-fruits').html(`${regionData.productsProduction.fruits.toLocaleString()}kg`);
+        //$('#productsProduction-mushrooms').html(`${regionData.productsProduction.mushrooms.toLocaleString()}kg`);
     });
 });
